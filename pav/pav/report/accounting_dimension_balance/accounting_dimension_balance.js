cc888119f9ae12a3b0e54c1c0fc77ea5187ae005
frappe <frappe@ubuntu.vm>
2021-04-27 20:22:03 +0000

    enhancement in Accounting Dimension Balance

--- pav/pav/report/accounting_dimension_balance/accounting_dimension_balance.js
+++ pav/pav/report/accounting_dimension_balance/accounting_dimension_balance.js
@@ -19,12 +19,12 @@ frappe.query_reports["Accounting Dimension Balance"] = {
 			"options": "Fiscal Year",
 			"default": frappe.defaults.get_user_default("fiscal_year"),
 			"reqd": 1,
-			"on_change": function(query_report) {
+			"on_change": function (query_report) {
 				var fiscal_year = query_report.get_values().fiscal_year;
 				if (!fiscal_year) {
 					return;
 				}
-				frappe.model.with_doc("Fiscal Year", fiscal_year, function(r) {
+				frappe.model.with_doc("Fiscal Year", fiscal_year, function (r) {
 					var fy = frappe.model.get_doc("Fiscal Year", fiscal_year);
 					frappe.query_report.set_filter_value({
 						from_date: fy.year_start_date,
@@ -52,30 +52,72 @@ frappe.query_reports["Accounting Dimension Balance"] = {
 			options: ["Cost Center", "Project"],
 			default: "Cost Center",
 			reqd: 1,
-			on_change: function() {
+			on_change: function () {
 				frappe.query_report.set_filter_value("budget_against_filter", []);
-				frappe.query_report.refresh();
+				frappe.query_report.refresh();				
+				// let budget_against = frappe.query_report.get_filter_value('budget_against');
+				// if (!budget_against) return;
+
+				// if(budget_against == "Task"){
+				// 	frappe.query_reports["Accounting Dimension Balance"].filters[6].options
+				// }
 			}
 		},
 		{
-			fieldname:"budget_against_filter",
+			fieldname: "budget_against_filter",
 			label: __('Dimension Filter'),
 			fieldtype: "MultiSelectList",
-			get_data: function(txt) {
+			get_data: function (txt) {
 				if (!frappe.query_report.filters) return;
 
 				let budget_against = frappe.query_report.get_filter_value('budget_against');
 				if (!budget_against) return;
 
+				console.log(" budget_against" + budget_against + " txt" + txt)
 				return frappe.db.get_link_options(budget_against, txt);
 			}
 		},
 		{
 			fieldname: "root_type",
 			label: __("Root Type"),
-			fieldtype: "Select",
-			options: ["", "Asset", "Liability", "Income", "Expense"],
-			default: "Expense",			
+			fieldtype: "MultiSelectList",
+			options: [__("Asset"), "Liability", "Income", "Expense"],
+			default: "Expense",
+			on_change: function () {
+				frappe.query_report.set_filter_value("account", []);
+				frappe.query_report.refresh();
+			}
+		},
+		{
+			fieldname: "account",
+			label: __("Account"),
+			fieldtype: "Link",
+			options: "Account",
+			get_query: function () {
+				var root = frappe.query_report.get_filter_value('root_type');				
+				if (root.length<1) {					
+					return {
+						filters: [
+							["Account", "company", "=", frappe.query_report.get_filter_value('company')]
+						]
+					}
+				}
+				else {
+					return {
+						filters: [
+							["Account", "root_type", "in", root],
+							["Account", "company", "=", frappe.query_report.get_filter_value('company')]
+						]
+					}
+				}
+			}
+		},
+		{
+			fieldname: "project",
+			label: __("Project"),
+			fieldtype: "Link",
+			options: "Project",
+			depends_on: 'eval:doc.budget_against=="Task"'
 		}
 	]
 };
--- pav/pav/report/accounting_dimension_balance/accounting_dimension_balance.py
+++ pav/pav/report/accounting_dimension_balance/accounting_dimension_balance.py
@@ -17,6 +17,7 @@ def execute(filters=None):
 		dimensions = get_cost_centers(filters)
 	
 	dimension_target_details = get_dimension_target_details(dimensions,filters)	
+	balances = get_opening_balances(dimensions,filters)	
 	#frappe.msgprint("{0}".format(dimension_target_details))
 	for ccd in dimension_target_details:
 		if ccd.debit>ccd.credit:
@@ -28,20 +29,63 @@ def execute(filters=None):
 		else:
 			debit=0.0
 			credit=0.0
-		data.append([ccd.budget_against, ccd.budget_against_name, ccd.debit, ccd.credit, debit, credit])
+		
+		opening_credit = 0
+		opening_debit = 0
+		for b in balances:
+			if b.budget_against == ccd.budget_against:
+				opening_credit = b.credit
+				opening_debit = b.debit
+		
+		if opening_debit>opening_credit:
+			opening_debit=opening_debit-opening_credit
+			opening_credit=0
+			dor=1
+		elif opening_debit<opening_credit:
+			opening_debit=0
+			opening_credit=opening_credit-opening_debit						
+			dor=0
+		else:
+			opening_debit=0
+			opening_credit=0
+			dor=2
+		if dor==1:
+			closing=(debit+credit)+(opening_debit)
+		else:
+			closing=(debit+credit)-(opening_debit)
+		if closing>0:
+			debit=closing
+			credit=0.0
+		elif closing<0:
+			debit=0.0
+			credit=closing
+		else:
+			debit=0.0
+			credit=0.0
+
+		if filters.get("budget_against")=="Task":
+			data.append([ccd.budget_against, ccd.budget_against_name, ccd.project, opening_debit, opening_credit, ccd.debit, ccd.credit, debit, credit])			
+		else:
+			data.append([ccd.budget_against, ccd.budget_against_name, opening_debit, opening_credit, ccd.debit, ccd.credit, debit, credit])
 
 	
 	return columns, data
 
 def get_columns(filters):
 	columns = [
-		_(filters.get("budget_against")) + ":Link/%s:150" % (filters.get("budget_against")),
-		_(filters.get("budget_against")+" Name") + ":Link/%s:300" % (filters.get("budget_against"))
-	]	
-	columns.append("Debit:Currency:150")
-	columns.append("Credit:Currency:150")
-	columns.append("Balance Debit:Currency:150")
-	columns.append("Balance Credit:Currency:150")
+		_(filters.get("budget_against")) + ":Link/%s:120" % (filters.get("budget_against"))		
+	]
+	if filters.budget_against == "Task":
+		columns.append(_("Subject") + ":Data:200")
+		columns.append(_("Project") + ":Link/Project:100")		
+	else:
+		columns.append(_(filters.get("budget_against")+" Name") + ":Data:200")
+	columns.append("Opening (Dr)):Currency:120")
+	columns.append("Opening (Cr):Currency:120")
+	columns.append("Debit:Currency:120")
+	columns.append("Credit:Currency:120")
+	columns.append("Closing (Dr):Currency:120")
+	columns.append("Closing (Cr):Currency:120")
 	return columns
 
 def get_cost_centers(filters):
@@ -73,17 +117,30 @@ def get_cost_centers(filters):
 def get_dimension_target_details(dimensions,filters):
 	budget_against = frappe.scrub(filters.get("budget_against"))
 	cond = ""
+	col = """ bal.{budget_against}_name """
+	if filters.budget_against == "Task" and filters.get('project'):
+	 	cond += """and bal.project = %s""" % (frappe.db.escape(filters.get('project')))
+
+	if filters.budget_against == "Task":
+		col= """ bal.project as project, bal.subject """
+	else:
+		col= """ bal.{budget_against}_name """
+		col = """ bal.%s_name """ % (budget_against)
+	if filters.get('account'):
+		cond += """and acc.name = %s""" % (frappe.db.escape(filters.get('account')))
+
 	if dimensions:
 		cond += """ and b.{budget_against} in (%s)""".format(
 			budget_against=budget_against) % ", ".join(["%s"] * len(dimensions))
 		if filters.root_type:
-			cond+= " and acc.root_type =  '{0}' ".format(filters.root_type)
+			cond+= " and acc.root_type in (%s)" % ( ", ".join(["%s"] * len(filters.root_type)))
+			dimensions += filters.root_type
 
 	return frappe.db.sql(
 		"""
 			select
 				b.{budget_against} as budget_against,
-				bal.{budget_against}_name as budget_against_name,
+				{col} as budget_against_name,
 				sum(b.debit) as debit,
 				sum(b.credit) as credit
 			from
@@ -91,8 +148,55 @@ def get_dimension_target_details(dimensions,filters):
 			INNER JOIN `tab{budget_against_label}` bal on b.{budget_against}=bal.name
 			INNER JOIN `tabAccount` acc on b.account=acc.name 
 			where				
-				b.company = %s 
+				b.company = %s
 				and b.posting_date between %s and %s 
+				{cond}
+			group by
+				b.{budget_against}
+		""".format(
+			budget_against_label=filters.budget_against,
+			budget_against=budget_against,
+			cond=cond,
+			col=col
+		),
+		tuple(
+			[
+				filters.company,
+				filters.from_date,
+				filters.to_date,
+			]
+			+ dimensions
+		), as_dict=True)
+
+def get_opening_balances(dimensions,filters):
+	budget_against = frappe.scrub(filters.get("budget_against"))
+	cond = ""
+	if filters.budget_against == "Task" and filters.get('project'):
+	 	cond += """and bal.project = %s""" % (frappe.db.escape(filters.get('project')))
+
+	if filters.get('account'):
+		cond += """and acc.name = %s""" % (frappe.db.escape(filters.get('account')))
+
+	if dimensions:
+		cond += """ and b.{budget_against} in (%s)""".format(
+			budget_against=budget_against) % ", ".join(["%s"] * len(dimensions))
+		if filters.root_type:
+			cond+= " and acc.root_type in (%s)" % ( ", ".join(["%s"] * len(filters.root_type)))
+			dimensions += filters.root_type
+
+	return frappe.db.sql(
+		"""
+			select
+				b.{budget_against} as budget_against,				
+				sum(b.debit) as debit,
+				sum(b.credit) as credit
+			from
+				`tabGL Entry` b	 
+			INNER JOIN `tab{budget_against_label}` bal on b.{budget_against}=bal.name
+			INNER JOIN `tabAccount` acc on b.account=acc.name 
+			where				
+				b.company = %s
+				and b.posting_date < %s 
 				{cond}
 			group by
 				b.{budget_against}
@@ -105,7 +209,6 @@ def get_dimension_target_details(dimensions,filters):
 			[
 				filters.company,
 				filters.from_date,
-				filters.to_date	
 			]
 			+ dimensions
 		), as_dict=True)
