809eb3b83fcc423782e9ed41fb418fe8e1cda4ea
frappe <frappe@ubuntu.vm>
2021-07-12 03:33:21 +0000

    add taxs to expense entry

---
+++ pav/pav/doctype/expense_entry_taxes_and_charges/expense_entry_taxes_and_charges.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Ahmed Mohammed Alkuhlani and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class ExpenseEntryTaxesandCharges(Document):
+	pass
